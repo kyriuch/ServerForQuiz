08b7b6f7d475b2668206311481ac912d99065032
Tomek <kyriuch@gmail.com>
2017-05-06 00:47:04 +0300

    Made server a console app

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,30 +1,13 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class Main extends Application {
+public class Main {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Server for Quiz");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
 
+    public static void main(String[] args) throws IOException, IllegalAccessException, InstantiationException, ClassNotFoundException {
         Injector injector = new Injector();
-
         ((ThreadsManager) injector.get(ThreadsManager.class)).addNewThread(new ClientsListener());
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        launch(args);
     }
 }
